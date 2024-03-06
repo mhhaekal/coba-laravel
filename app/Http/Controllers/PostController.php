7b54115285c5a4efc0126eb928fdfcdbc432d63a
mhhaekal <mhaekal55@gmail.com>
2024-03-06 17:00:09 +0700

    fix ui

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,8 @@ class PostController extends Controller
     {
         return view('posts', [
             "title" => "All Posts",
-            "posts" => Post::with(['author', 'category'])->latest()->get()
+            "active" => "posts",
+            "posts" => Post::latest()->get()
         ]);
     }
 
@@ -19,6 +20,7 @@ class PostController extends Controller
     {
         return view('post', [
             "title" => "ini single post",
+            "active" => "posts",
             "post" => $post
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "MH Haekal",
         "email" => "mhaekal55@gmail.com",
         "image" => "111.png"
@@ -29,6 +31,7 @@ Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 ROute::get('/categories', function () {
     return view('categories', [
         "title" => "Posts Categories",
+        "active" => "categories",
         "categories" => Category::all()
     ]);
 });
@@ -36,6 +39,7 @@ ROute::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post by Category : $category->name",
+        "active" => "categories",
         'posts' => $category->posts->load('category', 'author'),
     ]);
 });
