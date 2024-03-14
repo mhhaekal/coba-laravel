f87e3fa027c4034cb6c20a8fadc1d60f54970df4
mhhaekal <mhaekal55@gmail.com>
2024-03-14 13:01:56 +0700

    13 Searching and Pagination

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,9 +7,9 @@
             <h1 class="mb-3">{{ $post->title }}</h1>
                 <p>
                     By. 
-                    <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> 
+                    <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> 
                     in 
-                    <a href="/categories/{{$post->category->name}}">{{ $post->category->name }}</a>
+                    <a href="/posts?category={{$post->category->name}}">{{ $post->category->name }}</a>
                 </p>
 
                 <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ Route::get('/categories/{category:slug}', function (Category $category) {
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         'title' => "Post By Author : $author->name",
+        'active' => 'post',
         'posts' => $author->posts->load('category', 'author'),
     ]);
 });
